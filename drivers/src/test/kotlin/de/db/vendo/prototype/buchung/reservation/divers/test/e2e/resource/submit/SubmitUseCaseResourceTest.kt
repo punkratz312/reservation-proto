47020be180e95412df48ae12d9fb0faaa35ae685
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-03 11:04:21 +0100

    notNullValue

--- drivers/src/test/kotlin/de/db/vendo/prototype/buchung/reservation/divers/test/e2e/resource/submit/SubmitUseCaseResourceTest.kt
+++ drivers/src/test/kotlin/de/db/vendo/prototype/buchung/reservation/divers/test/e2e/resource/submit/SubmitUseCaseResourceTest.kt
@@ -3,15 +3,16 @@ package de.db.vendo.prototype.buchung.reservation.divers.test.e2e.resource.submi
 import de.db.vendo.prototype.buchung.BDD
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.notNullValue
 
 @QuarkusTest
 class SubmitUseCaseResourceTest : BDD() {
   override fun then_() {
     given()
-      .`when`().get("submit")
+      .`when`()
+      .get("submit")
       .then()
       .statusCode(200)
-      .body(equalTo("Hello from RESTEasy Reactive"))
+      .body(notNullValue())
   }
 }
