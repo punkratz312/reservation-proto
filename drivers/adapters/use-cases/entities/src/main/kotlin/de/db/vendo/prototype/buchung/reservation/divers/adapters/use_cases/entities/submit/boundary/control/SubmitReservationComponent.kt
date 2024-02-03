c6502808000bbf4c983a66db50aff8295085cb6b
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-03 10:33:14 +0100

    beans.xml

--- drivers/adapters/use-cases/entities/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/use_cases/entities/submit/boundary/control/SubmitReservationComponent.kt
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/use_cases/entities/submit/boundary/control/SubmitReservationComponent.kt
@@ -1,19 +1,18 @@
 package de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.submit.boundary.control
 
 import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.control.Component
-import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.reservation.boundary.control.ReservationRepository
 import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.reservation.boundary.control.entity.ReservationId
-import jakarta.enterprise.inject.Default
-import jakarta.inject.Inject
+import jakarta.enterprise.context.ApplicationScoped
 
+@ApplicationScoped
 class SubmitReservationComponent : Component<ReservationId> {
 
-    @Inject
-    @field: Default
-    lateinit var reservationRepository: ReservationRepository
+//  @Inject
+//  @field: Default
+//  lateinit var reservationRepository: ReservationRepository
 
-    override fun execute(reference: ReservationId) {
-        reservationRepository.save(reservationRepository.findBy(reference))
-    }
+  override fun execute(reference: ReservationId) {
+//    reservationRepository.save(reservationRepository.findBy(reference))
+  }
 
 }
