911979aa695cba4142115bc3309a719ee242869c
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-03 10:22:46 +0100

    beans.xml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 pluginManagement {
-    val quarkusPluginVersion: String by settings
-    val quarkusPluginId: String by settings
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        mavenLocal()
-    }
-    plugins {
-        id(quarkusPluginId) version quarkusPluginVersion
-    }
+  val quarkusPluginVersion: String by settings
+  val quarkusPluginId: String by settings
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    mavenLocal()
+  }
+  plugins {
+    id(quarkusPluginId) version quarkusPluginVersion
+  }
 }
 rootProject.name = "reservierung-prototype"
 include(":common:")
