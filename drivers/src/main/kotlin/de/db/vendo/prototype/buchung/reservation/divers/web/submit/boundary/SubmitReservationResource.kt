d40642f497d6813f206de73db9b0f2f5db0bfe89
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-03 22:08:07 +0100

    ReservationReq

--- drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/web/submit/boundary/SubmitReservationResource.kt
+++ drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/web/submit/boundary/SubmitReservationResource.kt
@@ -2,6 +2,7 @@ package de.db.vendo.prototype.buchung.reservation.divers.web.submit.boundary
 
 import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.Service
 import de.db.vendo.prototype.buchung.reservation.divers.adapters.submit.boundary.SubmitReservationResourceAdapter
+import de.db.vendo.prototype.buchung.reservation.divers.web.submit.boundary.control.entity.ReservationReq
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Default
 import jakarta.inject.Inject
@@ -9,11 +10,10 @@ import jakarta.ws.rs.PUT
 import jakarta.ws.rs.Path
 import org.jboss.resteasy.reactive.ResponseStatus
 import org.jboss.resteasy.reactive.RestResponse.StatusCode.NO_CONTENT
-import java.util.*
 
 @ApplicationScoped
 @Path("submit")
-class SubmitReservationResource : Service<UUID> {
+class SubmitReservationResource : Service<ReservationReq> {
 
   @Inject
   @field: Default
@@ -21,7 +21,7 @@ class SubmitReservationResource : Service<UUID> {
 
   @PUT
   @ResponseStatus(NO_CONTENT)
-  override fun serve(reference: UUID) {
-    resourceAdapter.serve(UUID.randomUUID())
+  override fun serve(reference: ReservationReq) {
+    resourceAdapter.serve(reference.id)
   }
 }
