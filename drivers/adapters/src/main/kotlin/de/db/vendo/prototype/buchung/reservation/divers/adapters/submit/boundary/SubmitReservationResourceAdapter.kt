fd92e9eec430238c5fe3dcc5db6e24b33ca735a9
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-03 21:59:14 +0100

    Submit

--- drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/submit/boundary/SubmitReservationResourceAdapter.kt
+++ drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/submit/boundary/SubmitReservationResourceAdapter.kt
@@ -1,19 +1,22 @@
 package de.db.vendo.prototype.buchung.reservation.divers.adapters.submit.boundary
 
-import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.FunctionalService
+import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.Service
+import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.submit.boundary.SubmitUseCase
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.inject.Default
+import jakarta.inject.Inject
 import java.util.*
 
+
 @ApplicationScoped
-class SubmitReservationResourceAdapter : FunctionalService<UUID, String> {
+class SubmitReservationResourceAdapter : Service<UUID> {
 
-//    @Inject
-//    @field: Default
-//    lateinit var submit: SubmitUseCase
+  @Inject
+  @field: Default
+  lateinit var submit: SubmitUseCase
 
-  override fun serve(reference: UUID): String {
-//        submit.action(reference)
-    return "SubmitReservationResourceAdapter$reference"
+  override fun serve(reference: UUID) {
+    return submit.action(reference)
   }
 
 }
--- drivers/adapters/use-cases/entities/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/use_cases/entities/submit/boundary/control/SubmitReservationComponent.kt
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/use_cases/entities/submit/boundary/control/SubmitReservationComponent.kt
@@ -1,18 +1,20 @@
 package de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.submit.boundary.control
 
 import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.control.Component
+import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.reservation.boundary.control.ReservationRepository
 import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.reservation.boundary.control.entity.ReservationId
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.inject.Default
+import jakarta.inject.Inject
 
 @ApplicationScoped
 class SubmitReservationComponent : Component<ReservationId> {
 
-//  @Inject
-//  @field: Default
-//  lateinit var reservationRepository: ReservationRepository
+  @Inject
+  @field: Default
+  lateinit var reservationRepository: ReservationRepository
 
   override fun execute(reference: ReservationId) {
-//    reservationRepository.save(reservationRepository.findBy(reference))
+    reservationRepository.save(reservationRepository.findBy(reference).copy(status = "Submit"))
   }
-
 }
