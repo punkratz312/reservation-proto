33f38bdb4738c9348c71239edcc2b586703d399e
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-03 22:02:14 +0100

    wip

--- drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/web/submit/boundary/SubmitReservationResource.kt
+++ drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/web/submit/boundary/SubmitReservationResource.kt
@@ -1,24 +1,27 @@
 package de.db.vendo.prototype.buchung.reservation.divers.web.submit.boundary
 
-import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.FunctionalService
+import de.db.vendo.prototype.buchung.reservation.common.bce.boundary.Service
 import de.db.vendo.prototype.buchung.reservation.divers.adapters.submit.boundary.SubmitReservationResourceAdapter
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Default
 import jakarta.inject.Inject
 import jakarta.ws.rs.PUT
 import jakarta.ws.rs.Path
+import org.jboss.resteasy.reactive.ResponseStatus
+import org.jboss.resteasy.reactive.RestResponse.StatusCode.NO_CONTENT
 import java.util.*
 
 @ApplicationScoped
 @Path("submit")
-class SubmitReservationResource : FunctionalService<UUID, String> {
+class SubmitReservationResource : Service<UUID> {
 
   @Inject
   @field: Default
   lateinit var resourceAdapter: SubmitReservationResourceAdapter
 
   @PUT
-  override fun serve(reference: UUID): String {
-    return resourceAdapter.serve(UUID.randomUUID())
+  @ResponseStatus(NO_CONTENT)
+  override fun serve(reference: UUID) {
+    resourceAdapter.serve(UUID.randomUUID())
   }
 }
