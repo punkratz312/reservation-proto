d305688457e0fb2676191760cb2d563ad3ff0793
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-09 08:47:07 +0100

    wip

--- drivers/src/test/kotlin/de/db/vendo/prototype/buchung/reservation/divers/test/e2e/resource/reset/failure/ResetWithoutSubmitTest.kt
+++ drivers/src/test/kotlin/de/db/vendo/prototype/buchung/reservation/divers/test/e2e/resource/reset/failure/ResetWithoutSubmitTest.kt
@@ -3,10 +3,11 @@ package de.db.vendo.prototype.buchung.reservation.divers.test.e2e.resource.reset
 import de.db.vendo.prototype.buchung.BDD
 import io.quarkus.test.junit.QuarkusTest
 import org.jboss.resteasy.reactive.RestResponse.StatusCode.NOT_FOUND
+import java.util.UUID.randomUUID
 
 @QuarkusTest
 class ResetWithoutSubmitTest : BDD() {
   override fun then_() {
-    resetReq("").statusCode(NOT_FOUND)
+    resetReq(randomUUID().toString()).statusCode(NOT_FOUND)
   }
 }
