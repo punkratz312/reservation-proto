5b7d1e04f6ea6217ed25f52fc61c569a72733ce9
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-09 14:54:07 +0100

    wip

--- drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/persistence/boundary/ReservationRepositoryAdapter.kt
+++ drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/persistence/boundary/ReservationRepositoryAdapter.kt
@@ -1,5 +1,36 @@
 package de.db.vendo.prototype.buchung.reservation.divers.adapters.persistence.boundary
 
-import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.persistence.boundary.ReservationRepositoryUseCase
+import de.db.vendo.prototype.buchung.reservation.divers.adapters.persistence.boundary.bridge.ReservationRepositoryAdapterBridge
+import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.persistence.boundary.bridge.ReservationRepositoryUseCaseBridge
+import jakarta.enterprise.inject.Default
+import jakarta.inject.Inject
+import java.util.*
 
-abstract class ReservationRepositoryAdapter : ReservationRepositoryUseCase
+class ReservationRepositoryAdapter : ReservationRepositoryAdapterBridge {
+
+  @Inject
+  @field: Default
+  lateinit var repositoryUseCaseBridge: ReservationRepositoryUseCaseBridge
+
+
+  override fun findBy(input: UUID): Optional<String> {
+    return repositoryUseCaseBridge.findBy(input).map { it.toString() }
+  }
+
+  override fun save(reservation: String): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun submit(input: UUID) {
+    TODO("Not yet implemented")
+  }
+
+  override fun confirm(input: UUID) {
+    TODO("Not yet implemented")
+  }
+
+  override fun reset(input: UUID) {
+    TODO("Not yet implemented")
+  }
+
+}
