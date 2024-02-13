4db062de6cab7a59735b02b4534db558f3e9cfbb
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-13 16:47:00 +0100

    wip

---
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/use_cases/entities/reservation/boundary/control/entity/ReservationDto.kt
@@ -0,0 +1,8 @@
+package de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.entities.reservation.boundary.control.entity
+
+import java.util.*
+
+data class ReservationDto(val id: UUID, val status: String)
+//data class Reservation(val id: UUID, val trip: Trip, val status: String)
+
+
