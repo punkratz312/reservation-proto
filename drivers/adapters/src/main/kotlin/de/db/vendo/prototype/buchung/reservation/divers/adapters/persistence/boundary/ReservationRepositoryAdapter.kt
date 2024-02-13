739f1d4987093dcd791d2703ed5ea83643e45fd3
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-13 08:55:16 +0100

    wip

--- drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/persistence/boundary/ReservationRepositoryAdapter.kt
+++ drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/adapters/persistence/boundary/ReservationRepositoryAdapter.kt
@@ -1,35 +1,15 @@
 package de.db.vendo.prototype.buchung.reservation.divers.adapters.persistence.boundary
 
 import de.db.vendo.prototype.buchung.reservation.divers.adapters.persistence.boundary.bridge.ReservationRepositoryAdapterBridge
-import de.db.vendo.prototype.buchung.reservation.divers.adapters.use_cases.persistence.boundary.bridge.ReservationRepositoryUseCaseBridge
+import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Default
 import jakarta.inject.Inject
-import java.util.*
 
-class ReservationRepositoryAdapter : ReservationRepositoryAdapterBridge {
+@ApplicationScoped
+class ReservationRepositoryAdapter {
 
   @Inject
   @field: Default
-  lateinit var bridge: ReservationRepositoryUseCaseBridge
-
-  override fun findBy(input: UUID): Optional<String> {
-    return bridge.findBy(input)
-  }
-
-  override fun save(reservation: String): String {
-    return bridge.save(reservation)
-  }
-
-  override fun submit(input: UUID) {
-    TODO("Not yet implemented")
-  }
-
-  override fun confirm(input: UUID) {
-    TODO("Not yet implemented")
-  }
-
-  override fun reset(input: UUID) {
-    TODO("Not yet implemented")
-  }
+  lateinit var bridge: ReservationRepositoryAdapterBridge
 
 }
--- drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/persistence/boundary/ReservationRepositoryDriver.kt
+++ drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/persistence/boundary/ReservationRepositoryDriver.kt
@@ -2,16 +2,18 @@ package de.db.vendo.prototype.buchung.reservation.divers.persistence.boundary
 
 import de.db.vendo.prototype.buchung.reservation.divers.adapters.persistence.boundary.bridge.ReservationRepositoryAdapterBridge
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepository
+import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Default
 import jakarta.inject.Inject
 import java.util.*
 
+@ApplicationScoped
 class ReservationRepositoryDriver : ReservationRepositoryAdapterBridge {
 
   @Inject
   @field: Default
   lateinit var panacheRepository: PanacheRepository<String>
-  
+
   override fun findBy(input: UUID): Optional<String> {
     TODO("Not yet implemented")
   }
