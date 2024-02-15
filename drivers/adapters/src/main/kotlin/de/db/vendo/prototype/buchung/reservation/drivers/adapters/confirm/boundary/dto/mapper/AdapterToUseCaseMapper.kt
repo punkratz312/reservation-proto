c709d41a2cafbad6aafd6df831eca2d4e5a9cd76
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-15 20:11:27 +0100

    .mapper

--- drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/drivers/adapters/confirm/boundary/dto/mapper/AdapterToUseCaseMapper.kt
+++ drivers/adapters/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/drivers/adapters/confirm/boundary/dto/mapper/AdapterToUseCaseMapper.kt
@@ -1,5 +1,6 @@
-package de.db.vendo.prototype.buchung.reservation.drivers.adapters.confirm.boundary.dto
+package de.db.vendo.prototype.buchung.reservation.drivers.adapters.confirm.boundary.dto.mapper
 
+import de.db.vendo.prototype.buchung.reservation.drivers.adapters.confirm.boundary.dto.ConfirmReservationResourceAdapterDTO
 import de.db.vendo.prototype.buchung.reservation.drivers.adapters.use_cases.confirm.boundary.dto.ReservationDTO
 import org.mapstruct.Mapper
 
--- drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/drivers/web/confirm/boundary/dto/mapper/ConfirmReservationResourceDTOMapper.kt
+++ drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/drivers/web/confirm/boundary/dto/mapper/ConfirmReservationResourceDTOMapper.kt
@@ -1,6 +1,7 @@
-package de.db.vendo.prototype.buchung.reservation.drivers.web.confirm.boundary.dto
+package de.db.vendo.prototype.buchung.reservation.drivers.web.confirm.boundary.dto.mapper
 
 import de.db.vendo.prototype.buchung.reservation.drivers.adapters.confirm.boundary.dto.ConfirmReservationResourceAdapterDTO
+import de.db.vendo.prototype.buchung.reservation.drivers.web.confirm.boundary.dto.ConfirmReservationResourceDTO
 import org.mapstruct.Mapper
 
 @Mapper
