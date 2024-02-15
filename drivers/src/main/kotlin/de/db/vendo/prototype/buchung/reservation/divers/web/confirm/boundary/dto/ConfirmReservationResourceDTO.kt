04f659682ae2a027a27bf8ec920fd8f0d032c272
Vincent Potucek <vincent.potucek@deutschebahn.com>
2024-02-15 19:58:44 +0100

    wip

--- drivers/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/divers/web/confirm/boundary/dto/ConfirmReservationResourceDTO.kt
+++
@@ -1,5 +0,0 @@
-package de.db.vendo.prototype.buchung.reservation.drivers.web.confirm.boundary.dto
-
-import java.util.*
-
-data class ConfirmReservationResourceDTO(val id: UUID, val status: String)
