64fa5ad670078b77aeb479228e7be47e6fe66ece
Vincent Potucek <vincent.potucek@udg.de>
2024-05-24 23:23:36 +0200

    wip

--- drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/control/FindAllHawksComponent.kt
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/control/FindAllHawksComponent.kt
@@ -2,15 +2,33 @@ package de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.wood
 
 import de.company.prototype.woodpecker.common.bce.boundary.control.OutputComponent
 import de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity.Woodpecker
+import de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity.WoodpeckerData
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import java.io.File
 
 @ApplicationScoped
 class FindAllHawksComponent @Inject constructor(
-  private val woodpeckers: Woodpeckers
 ) : OutputComponent<Set<Woodpecker>> {
 
   override fun execute(): Set<Woodpecker> {
-    return woodpeckers.findAllHawks()
+    val lineList = mutableListOf<String>()
+    val result = mutableListOf<Woodpecker>()
+    File("birds.csv").useLines { lines -> lines.forEach { lineList.add(it) } }
+    lineList.forEach { line ->
+      println(">  $line")
+      val parts = line.split(";")
+      if (parts.size == 4) {
+        val woodpeckerData = WoodpeckerData(
+          birdFamily = parts[0],
+          englishBirdName = parts[1],
+          scientificBirdName = parts[2],
+          iucnCategory = parts[3]
+        )
+        val woodpecker = Woodpecker(woodpeckerData)
+        result.add(woodpecker)
+      }
+    }
+    return result.toSet()
   }
 }
