f468476bdde465ca0fb3d8ffdaeaf4e06d732a9e
Vincent Potucek <vincent.potucek@udg.de>
2024-05-24 08:50:39 +0200

    Woodpecker

---
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/db/vendo/prototype/buchung/reservation/drivers/adapters/use_cases/entities/woodpecker/boundary/control/entity/WoodpeckerData.kt
@@ -0,0 +1,5 @@
+package de.db.vendo.prototype.buchung.reservation.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity
+
+import java.util.*
+
+data class WoodpeckerData(val id: UUID)
