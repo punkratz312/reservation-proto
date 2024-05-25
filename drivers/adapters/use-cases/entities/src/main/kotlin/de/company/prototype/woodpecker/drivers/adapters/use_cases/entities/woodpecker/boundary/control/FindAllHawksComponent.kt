145f032a0c28deb07cee2afdf36454873120be61
Vincent Potucek <vincent.potucek@udg.de>
2024-05-25 08:47:07 +0200

    wip

--- drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/control/FindAllHawksComponent.kt
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/control/FindAllHawksComponent.kt
@@ -3,6 +3,7 @@ package de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.wood
 import de.company.prototype.woodpecker.common.bce.boundary.control.OutputComponent
 import de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity.Woodpecker
 import de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity.WoodpeckerData
+import jakarta.annotation.PostConstruct
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
 import java.io.File
@@ -11,19 +12,32 @@ import java.io.File
 class FindAllHawksComponent @Inject constructor(
 ) : OutputComponent<Set<Woodpecker>> {
 
-  override fun execute(): Set<Woodpecker> {
-    val result = mutableListOf<Woodpecker>()
+  private val result: MutableList<Woodpecker> = mutableListOf()
+
+  @PostConstruct
+  fun init(): Set<Woodpecker> {
     File("birds.csv").useLines { lines ->
-      lines.forEach {
-        val woodpecker = it.split(";")
-        result.add(Woodpecker(WoodpeckerData(
-          birdFamily = woodpecker[0],
-          englishBirdName = woodpecker[1],
-          scientificBirdName = woodpecker[2],
-          iucnCategory = woodpecker[3]
-        )))
+      lines.forEach { line ->
+        val woodpecker = line.split(";")
+        val birdFamily = woodpecker[0]
+        if (birdFamily == "Hawks") {
+          result.add(Woodpecker(WoodpeckerData(
+            birdFamily = birdFamily,
+            englishBirdName = woodpecker[1],
+            scientificBirdName = woodpecker[2],
+            iucnCategory = woodpecker[3]
+          )))
+        }
       }
     }
+    return result.sortedWith(compareBy {
+      listOf(
+        "LC", "NT", "VU", "EN", "CR", "EW", "EX", "DD"
+      ).indexOf(it.data.iucnCategory)
+    }).toSet();
+  }
+
+  override fun execute(): Set<Woodpecker> {
     return result.toSet()
   }
 }
