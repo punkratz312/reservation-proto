5187376a381b74df2d4ee813c2ee9c27dd01bd57
Vincent Potucek <vincent.potucek@udg.de>
2024-05-25 13:42:51 +0200

    wip

--- drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/control/FindAllHawksComponent.kt
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/control/FindAllHawksComponent.kt
@@ -7,50 +7,51 @@ import jakarta.annotation.PostConstruct
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
 import java.io.File
+import java.math.BigInteger.ZERO
 
 
 @ApplicationScoped
 class FindAllHawksComponent @Inject constructor(
 ) : OutputComponent<Set<Woodpecker>> {
 
-  private val hawks: MutableList<Woodpecker> = mutableListOf()
-  private var hawks2: Set<Woodpecker> = mutableSetOf()
+  private var hawks: Set<Woodpecker> = mutableSetOf()
   private val iucnOrder = setOf(
     "LC", "NT", "VU", "EN", "CR", "EW", "EX", "DD" // order effects sort evaluation
   )
   private val iucnCategoryToIndex = iucnCategoryToIndex()
   private val comparator = compareBy<Woodpecker> {
-    iucnCategoryToIndex[it.data.iucnCategory] ?: Int.MAX_VALUE
+    iucnCategoryToIndex[it.data.iucnCategory] ?: ZERO
   }
 
   @PostConstruct
   fun init() {
-    parseHawks()
-    hawks2 = this.hawks.sortedWith(comparator).toSet()
-    hawks2.forEach {
+    hawks = parseHawks().sortedWith(comparator).toSet()
+    hawks.forEach {
       println(it)
     }
   }
 
-  private fun parseHawks() {
-    File(javaClass.classLoader.getResource("/birds.csv")!!.file).useLines { lines ->
-      lines.forEach {
-        parseHawk(it)
-      }
+  private fun parseHawks(): MutableList<Woodpecker> {
+    File(javaClass.classLoader.getResource("birds.csv")!!.file).useLines { lines ->
+      return parseHawk(lines)
     }
   }
 
-  private fun parseHawk(line: String) {
-    val woodpecker = line.split(";")
-    val birdFamily = woodpecker[0]
-    if (birdFamily == "Hawks") {
-      hawks.add(Woodpecker(WoodpeckerData(
-        birdFamily = birdFamily,
-        englishBirdName = woodpecker[1],
-        scientificBirdName = woodpecker[2],
-        iucnCategory = woodpecker[3]
-      )))
+  private fun parseHawk(lines: Sequence<String>): MutableList<Woodpecker> {
+    val hawks: MutableList<Woodpecker> = mutableListOf()
+    lines.forEach {
+      val woodpecker = it.split(";")
+      val birdFamily = woodpecker[0]
+      if (birdFamily == "Hawks, eagles") {
+        hawks.add(Woodpecker(WoodpeckerData(
+          birdFamily = birdFamily,
+          englishBirdName = woodpecker[1],
+          scientificBirdName = woodpecker[2],
+          iucnCategory = woodpecker[3]
+        )))
+      }
     }
+    return hawks
   }
 
   private fun iucnCategoryToIndex(): Map<String, Int> {
