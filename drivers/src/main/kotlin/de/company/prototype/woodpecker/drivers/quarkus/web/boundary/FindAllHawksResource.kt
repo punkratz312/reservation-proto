62c50af051c9f5b50fed842acd7a8db602c21871
Vincent Potucek <vincent.potucek@udg.de>
2024-05-25 13:09:02 +0200

    wip

--- drivers/src/main/kotlin/de/company/prototype/woodpecker/drivers/quarkus/web/boundary/FindAllHawksResource.kt
+++ drivers/src/main/kotlin/de/company/prototype/woodpecker/drivers/quarkus/web/boundary/FindAllHawksResource.kt
@@ -1,21 +1,20 @@
 package de.company.prototype.woodpecker.drivers.quarkus.web.boundary
 
-import de.company.prototype.woodpecker.common.bce.boundary.FunctionalService
+import de.company.prototype.woodpecker.common.bce.boundary.OutputService
 import de.company.prototype.woodpecker.drivers.adapters.submit.boundary.FindAllHawksResourceAdapter
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody
 
 @ApplicationScoped
 @Path("woodpeckers/findAllHawks")
 class FindAllHawksResource @Inject constructor(
   private val resourceAdapter: FindAllHawksResourceAdapter
-) : FunctionalService<Unit, Any> {
+) : OutputService<Any> {
 
   @GET
-  override fun serve(@RequestBody input: Unit): Any {
+  override fun serve(): Any {
     return resourceAdapter.serve()
   }
 }
