0fcbd783bc2902edf185e86db126038bc3335b40
Vincent Potucek <vincent.potucek@udg.de>
2024-05-26 13:20:18 +0200

    woodpecker wip

--- drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/Woodpeckers.kt
+++ drivers/adapters/use-cases/entities/src/main/kotlin/de/company/prototype/woodpecker/drivers/adapters/use_cases/entities/woodpecker/boundary/Woodpeckers.kt
@@ -1,9 +1,9 @@
-package de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary
-
-import de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity.Woodpecker
-
-interface Woodpeckers {
-
-  fun findAllHawks(): Set<Woodpecker>
-
-}
+//package de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary
+//
+//import de.company.prototype.woodpecker.drivers.adapters.use_cases.entities.woodpecker.boundary.control.entity.Woodpecker
+//
+//interface Woodpeckers {
+//
+//  fun findAllHawks(): Set<Woodpecker>
+//
+//}
